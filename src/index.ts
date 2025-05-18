4cfb77631b5232cd1cf52657850b13738098e06f
shop4max <shop4max@gmail.com>
2025-05-18 15:43:25 -0700

    Update index.ts back

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cats",
+      prompt: "cyberpunk cat",
     };
 
     const response = await env.AI.run(
